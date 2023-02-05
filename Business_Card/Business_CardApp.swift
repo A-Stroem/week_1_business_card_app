9833363c790bd9e29cbf517e7a5533fd95c4e882
A-Stroem <asjnyborg@hotmail.com>
2023-02-05 22:42:52 +0100

    Initial Commit

---
+++ Business_Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business_Card
+//
+//  Created by Anders Strøm on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
